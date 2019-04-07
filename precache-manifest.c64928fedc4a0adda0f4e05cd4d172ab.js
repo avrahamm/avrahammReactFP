1cfb46a783863e73f403022dcac4f2393357cc17
avrahamm <papaabramg@gmail.com>
2019-04-07 17:24:34 +0300

    Updates

---
+++ precache-manifest.c64928fedc4a0adda0f4e05cd4d172ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "714cf6ed8a18a077cb46",
+    "url": "/avrahammReactFP/static/js/runtime~main.831544f7.js"
+  },
+  {
+    "revision": "95d1dd4e10da573fa4ad",
+    "url": "/avrahammReactFP/static/js/main.4349fc84.chunk.js"
+  },
+  {
+    "revision": "41a4e0d6cdec6d7afc68",
+    "url": "/avrahammReactFP/static/js/2.344cecb9.chunk.js"
+  },
+  {
+    "revision": "95d1dd4e10da573fa4ad",
+    "url": "/avrahammReactFP/static/css/main.b50ba3bf.chunk.css"
+  },
+  {
+    "revision": "8fd34f4087a6fecf039ff532d9fa7eb8",
+    "url": "/avrahammReactFP/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/avrahammReactFP/precache-manifest.c64928fedc4a0adda0f4e05cd4d172ab.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/avrahammReactFP/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
