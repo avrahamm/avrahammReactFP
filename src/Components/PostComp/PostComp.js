731dd9dc2e21d887d640973802100ccab8b46b29
avrahamm <papaabramg@gmail.com>
2022-06-19 21:11:51 +0300

    PostComp.js

--- src/Components/PostComp/PostComp.js
+++ src/Components/PostComp/PostComp.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './PostComp.css';
 
-let PostComp = (props) => {
-    // console.log("PostComp render userId = " + props.post.userId + " post Id = " + props.post.id );
+let PostComp = ({post}) => {
+    // console.log("PostComp render userId = " + post.userId + " post Id = " + post.id );
     return (
         <div className={"PostComp"}>
             <div className={"rowDiv"}>
                 <span className={"label"}> Title :</span>
-                <span className={"content"}>{props.post.title}</span>
+                <span className={"content"}>{post.title}</span>
             </div>
 
             <div className={"rowDiv"}>
                 <span className={"label"}> Body :</span>
-                <span className={"content"}>{props.post.body}</span>
+                <span className={"content"}>{post.body}</span>
             </div>
         </div>
     );
