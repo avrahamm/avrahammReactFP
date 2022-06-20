a5c55d17d336b5167fc044404a406ff8ef5748d3
avrahamm <papaabramg@gmail.com>
2022-06-20 13:49:32 +0300

    UserComp.js

--- src/Components/UserComp/UserComp.js
+++ src/Components/UserComp/UserComp.js
@@ -1,175 +1,120 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom' ;
+import * as React from 'react';
+import {useDispatch} from 'react-redux';
+import {Link} from 'react-router-dom';
 import './UserComp.css';
 
-class UserComp extends Component {
+export default function UserComp({userData}) {
+    const dispatch = useDispatch();
 
-    constructor(props)
-    {
-        super(props);
-        this.state ={
-            userId:0,
-            name:"",
-            email:"",
-            otherDataVisible: false,
-            street:"",
-            city:"",
-            zipcode:"",
-            tasksStatus:"completedTasks", // TODO! probably should stay in props
-            internalStateChange: false,
-            selectedUserId:0
-        };
-    }
+    let {
+        userId, tasksStatus, selectedUserId
+    } = userData;
 
-    static getDerivedStateFromProps(nextProps,prevState)
-    {
-        // State is changed from inside - not by props from parent.
-        // Return prevState as is with the internalStateChange flag reset.
-        if( prevState.internalStateChange){
-            return { internalStateChange:false};
-        }
-        else {
-            return {
-                userId: nextProps.userData.userId,
-                name: nextProps.userData.name,
-                email: nextProps.userData.email,
-                street: nextProps.userData.street,
-                city: nextProps.userData.city,
-                zipcode: nextProps.userData.zipcode,
-                tasksStatus: nextProps.userData.tasksStatus,
-                selectedUserId: nextProps.userData.selectedUserId
-            };
+    const [name,   setName]   = React.useState(userData.name);
+    const [email,  setEmail]  = React.useState(userData.email);
+    const [street, setStreet] = React.useState(userData.street);
+    const [city,   setCity]   = React.useState(userData.city);
+    const [zipcode,setZipcode]= React.useState(userData.zipcode);
+    const [otherDataVisible, setOtherDataVisible] = React.useState(false);
+
+    const showOtherData = (e) => {
+        if (!otherDataVisible) {
+            setOtherDataVisible(true);
         }
     }
 
-    showOtherData = ( ) =>
-    {
-       if( !this.state.otherDataVisible) {
-           //console.log("showOtherData");
-           this.setState({otherDataVisible: true, internalStateChange: true});
-       }
-    }
-
-    hideShowData  = ( ) =>
-    {
-        if( this.state.otherDataVisible) {
-            //console.log("hideShowData");
-            this.setState(curState =>
-            {
-                return {otherDataVisible: false, internalStateChange: true}
-            });
+    const hideShowData = (e) => {
+        if (otherDataVisible) {
+            setOtherDataVisible(false);
         }
     }
 
-    updateField = (e) =>
-    {
-        let inputName = e.target.name;
-        let value = e.target.value;
-        this.setState({[inputName]:value, internalStateChange: true});
+    // dispatching plain actions
+    const deleteUser = (userId) => {
+        dispatch({type: 'DELETE_USER', userId: userId})
     }
 
-    //@link:https://www.jstips.co/en/javascript/picking-and-rejecting-object-properties/
-    pickUpdatedKeysFromObject = (obj, keys) =>
-    {
-        return keys.map(k => k in obj ? {[k]: obj[k]} : {})
-            .reduce((res, o) => Object.assign(res, o), {});
+    const updateUser = () => {
+        let updatedUserData = {
+            userId,
+            name,
+            email,
+            street,
+            city,
+            zipcode,
+        }
+        dispatch({type: 'UPDATE_USER', updatedUserData});
     }
 
-    render() {
-        //console.log("UserComp render userId = " + this.state.userId);
-        let taskStatus = this.state.tasksStatus; //"uncompletedTasks";
-        const otherDataStyle = {
-            display:this.state.otherDataVisible ? "block" : "none"
-        };
-        let userSelected = (this.state.userId === this.state.selectedUserId) ? "userSelected" : "";
-        return(
+    const selectUser = (userId) => {
+        dispatch({type: 'SELECT_USER', userId: userId});
+    }
 
-            <div className={`UserComp ${taskStatus} ${userSelected}`} >
-                <Link to={`/user/${this.state.userId}`} >
-                    <div className={"rowDiv id"}
-                         onClick={() => this.props.selectUser(this.state.userId)}
-                    >
-                        <span className={"label"}> ID :</span>
-                        <span>{this.state.userId}</span>
-                    </div>
-                </Link>
-                <div className={"rowDiv name"}>
-                    <span className={"label"}> Name :</span>
-                        <input type="text" name={"name"}
-                               value={this.state.name}
-                                onChange={this.updateField}/>
+    //console.log("UserComp render userId = " + userId);
+    const otherDataStyle = {
+        display: otherDataVisible ? "block" : "none"
+    };
+    let userSelected = (userId === selectedUserId) ? "userSelected" : "";
+    return (
+        <div className={`UserComp ${tasksStatus} ${userSelected}`}>
+            <Link to={`/user/${userId}`}>
+                <div className={"rowDiv id"}
+                     onClick={() => selectUser(userId)}
+                >
+                    <span className={"label"}> ID :</span>
+                    <span>{userId}</span>
                 </div>
-                <div className={"rowDiv email"}>
-                    <span className={"label"}> Email :</span>
-                        <input type="text" name={"email"}
-                              value={this.state.email}
-                               onChange={this.updateField}/>
-                </div>
-
-                <div className={"buttonsAndOtherData"}>
-                    <input type="button" className={"otherDataButton  button"}
-                           onMouseOver={this.showOtherData}
-                           onClick={this.hideShowData}
-                           value={"Other Data"}/>
-
-                    <div className={"otherDataDiv"} style={otherDataStyle} >
-                        <div className={"rowDiv"}>
-                            <span className={"label"}> Street :</span>
-                            <input type="text" name={"street"} value={this.state.street} onChange={this.updateField}/>
-                        </div>
-                        <div className={"rowDiv"}>
-                            <span className={"label"}> City :</span>
-                            <input type="text" name={"city"} value={this.state.city} onChange={this.updateField}/>
-                        </div>
-                        <div className={"rowDiv"}>
-                            <span className={"label"}> Zipcode :</span>
-                            <input type="text" name={"zipcode"} value={this.state.zipcode} onChange={this.updateField}/>
-                        </div>
-                    </div>
-
-                    <input type="button" className={"userButton  button"}
-                           onClick={() => this.props.updateUser(this)}
-                           value={"Update"}
-                    />
-                    <input type="button" className={"userButton  button"}
-                           onClick={() => this.props.deleteUser(this.state.userId)}
-                           value={"Delete"}/>
-                </div>
+            </Link>
+            <div className={"rowDiv name"}>
+                <span className={"label"}> Name :</span>
+                <input type="text" name={"name"}
+                       value={name}
+                       onChange={e => setName(e.target.value) }
+                />
             </div>
-        );
+            <div className={"rowDiv email"}>
+                <span className={"label"}> Email :</span>
+                <input type="text" name={"email"}
+                       value={email}
+                       onChange={e => setEmail(e.target.value) }
+                />
+            </div>
 
-    }
+            <div className={"buttonsAndOtherData"}>
+                <input type="button" className={"otherDataButton  button"}
+                       onMouseOver={showOtherData}
+                       onClick={hideShowData}
+                       value={"Other Data"}/>
+
+                <div className={"otherDataDiv"} style={otherDataStyle}>
+                    <div className={"rowDiv"}>
+                        <span className={"label"}> Street :</span>
+                        <input type="text" name={"street"} value={street}
+                               onChange={e => setStreet(e.target.value) }
+                        />
+                    </div>
+                    <div className={"rowDiv"}>
+                        <span className={"label"}> City :</span>
+                        <input type="text" name={"city"} value={city}
+                               onChange={e => setCity(e.target.value) }
+                        />
+                    </div>
+                    <div className={"rowDiv"}>
+                        <span className={"label"}> Zipcode :</span>
+                        <input type="text" name={"zipcode"} value={zipcode}
+                               onChange={e => setZipcode(e.target.value) }
+                        />
+                    </div>
+                </div>
+
+                <input type="button" className={"userButton  button"}
+                       onClick={(e) => updateUser()}
+                       value={"Update"}
+                />
+                <input type="button" className={"userButton  button"}
+                       onClick={(e) => deleteUser(userId)}
+                       value={"Delete"}/>
+            </div>
+        </div>
+    );
 }
-
-const mapDispatchToProps = dispatch => {
-    return {
-        // dispatching plain actions
-        deleteUser: (userId) =>
-        {
-            dispatch({ type: 'DELETE_USER',userId:userId })
-        },
-        updateUser: (userObj ) =>
-        {
-            let updatedUserKeys =[
-                'userId',
-                'name',
-                'email',
-                'street',
-                'city',
-                'zipcode'
-            ];
-            let updatedUserData = userObj.pickUpdatedKeysFromObject(userObj.state,updatedUserKeys);
-            dispatch({ type : 'UPDATE_USER', updatedUserData : updatedUserData});
-        },
-        selectUser: (userId) =>
-        {
-            dispatch({ type : 'SELECT_USER', userId : userId});
-        }
-    }
-}
-
-export default (connect(
-    null,
-    mapDispatchToProps
-)(UserComp));
