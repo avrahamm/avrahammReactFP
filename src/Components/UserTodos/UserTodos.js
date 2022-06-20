f5f30821a21e5c443c1877de1f7f536c04ff8f84
avrahamm <papaabramg@gmail.com>
2022-06-20 14:33:08 +0300

    UserTodos.js

--- src/Components/UserTodos/UserTodos.js
+++ src/Components/UserTodos/UserTodos.js
@@ -1,71 +1,48 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom' ;
+import * as React from 'react';
+import {useSelector} from 'react-redux';
+import {Link, useHistory, useParams, useRouteMatch} from 'react-router-dom';
+
 import TodoComp from '../TodoComp/TodoComp';
 import './UserTodos.css';
 
-class UserTodos extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state= {userId:0, userTodos:[] };
+export default function UserTodos() {
+    const history = useHistory();
+    const match = useRouteMatch();
+    let {userId} = useParams();
+    userId = Number.parseInt(userId);
+
+    const selectedUserId = useSelector(state => state.selectedUserId);
+    //for direct loading, not by selecting user - wil be redirected.
+    if (!selectedUserId || // 0 means no user was selected - direct navigation.
+        (userId !== selectedUserId) // selected user deleted
+    ) {
+        history.push('/');
     }
 
-    static getDerivedStateFromProps(nextProps, prevState)
-    {
-        let userId = Number.parseInt(nextProps.match.params.userId);
-        let selectedUserId = nextProps.myCustomProps.selectedUserId;
-        //for direct loading not by selecting user - wil be redirected.
-        if( !selectedUserId || // 0 means no user was selected
-            (userId !== selectedUserId) //
-        ) {
-            nextProps.history.push('/');
+    const userTodos = useSelector(state => state.todos[userId]);
+
+    //console.log("UserTodos render");
+    let userTodosArray = userTodos.map((todo, index) => {
+            return <TodoComp key={index} todo={todo}/>;
         }
-        let userTodos = [];
-        if ( (nextProps.myCustomProps.todos.length) &&
-            Array.isArray(nextProps.myCustomProps.todos[userId]))
-        {
-            userTodos = nextProps.myCustomProps.todos[userId];
-        }
-        return {
-                userId: userId,
-                userTodos: userTodos,
-            };
-    }
+    );
+    return (
+        <div className="UserTodos">
+            <div className="UserTodosHeader">
+                <span className={"label"}>
+                    <h4>Todos - User {userId}</h4>
+                </span>
 
-    render() {
-
-        //console.log("UserTodos render");
-        let userTodosArray = this.state.userTodos.map( (todo, index) =>
-            {
-                return <TodoComp key={index} todo={todo} />;
-            }
-        );
-        return (
-            <div className="UserTodos">
-                <div className="UserTodosHeader">
-                    <span className={"label"}>
-                        <h4>Todos - User {this.state.userId}</h4>
-                    </span>
-
-                    <span className={"addTodoButton"}>
-                        <Link to={`${this.props.match.url}/addTodo`}>
-                            <input className={"button"}  type="button" value={"Add"} />
-                        </Link>
-                    </span>
-
-                </div>
-
-                <div className="UserTodosBody">
-                {userTodosArray}
-                </div>
+                <span className={"addTodoButton"}>
+                    <Link to={`${match.url}/addTodo`}>
+                        <input className={"button"} type="button" value={"Add"}/>
+                    </Link>
+                </span>
             </div>
-        );
-    }
-}
 
-const mapStateToProps = (state) => {
-    return {myCustomProps: state};
+            <div className="UserTodosBody">
+                {userTodosArray}
+            </div>
+        </div>
+    );
 }
-
-export default connect(mapStateToProps)(UserTodos);
