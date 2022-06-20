497c0ccd00a8ad809f6dee07c3f4e91fbb853e25
avrahamm <papaabramg@gmail.com>
2022-06-20 11:32:02 +0300

    ShowUsers.js

--- src/Components/ShowUsers/ShowUsers.js
+++ src/Components/ShowUsers/ShowUsers.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import * as React from 'react';
+import {useSelector} from 'react-redux';
 import UserComp from '../UserComp/UserComp';
 import './ShowUsers.css';
 import {filterFromUndefined} from "../../Utils/CommonUtils";
@@ -7,84 +7,53 @@ import {filterFromUndefined} from "../../Utils/CommonUtils";
 /**
  * Container connected to redux to pull, hold and render Users list.
  */
-class ShowUsers extends Component {
+export default function ShowUsers() {
+    const users = useSelector(state => filterFromUndefined(state.users));
+    const todos = useSelector(state => state.todos);
+    const selectedUserId = useSelector(state => state.selectedUserId);
+    const searchString = useSelector(state => state.searchString.toLowerCase());
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: [],
-            todos: [],
-            searchString:"",
-            selectedUserId:0
-        }
+    //console.log("ShowUsers Render()");
+    let userComps = [];
+    let filteredUsers = users;
+    let filterString = searchString;
+
+    if (filterString !== "") {
+        filteredUsers = filteredUsers.filter(function (user) {
+            return (
+                user.name.toLowerCase().includes(filterString) ||
+                user.email.toLowerCase().includes(filterString)
+            )
+        });
     }
 
-    static getDerivedStateFromProps(nextProps,prevState) {
-        return {
-            users: filterFromUndefined(nextProps.myCustomProps.users),
-            todos: nextProps.myCustomProps.todos,
-            selectedUserId: nextProps.myCustomProps.selectedUserId,
-            searchString: nextProps.myCustomProps.searchString
-        }
-    }
-
-    setSearchString = (e) =>
-    {
-        // case insensitive.
-        this.setState({searchString : e.target.value.toLowerCase()});
-    }
-
-    render() {
-        //console.log("ShowUsers Render()");
-        let userComps = [];
-        // let todos = this.state.todos;
-        let filteredUsers = this.state.users;
-        let filterString = this.state.searchString;
-
-        if( filterString !== "" ) {
-            filteredUsers = filteredUsers.filter(function (user) {
-                return (
-                    user.name.toLowerCase().includes(filterString) ||
-                    user.email.toLowerCase().includes(filterString)
-                )
+    userComps = filteredUsers.map((user, index) => {
+        let userId = user.id;
+        let userTasksStatus = "completedTasks";
+        if (Array.isArray(todos[userId])) {
+            let allTasksCompleted = todos[userId].every(function (todo) {
+                return (todo.completed === true);
             });
+            userTasksStatus = (allTasksCompleted === true) ? "completedTasks" : "uncompletedTasks";
         }
 
-        userComps = filteredUsers.map((user, index) => {
-            let userId = user.id;
-            let todos = this.state.todos;
-            let userTasksStatus = "completedTasks";
-            if (Array.isArray(todos[userId])) {
-                let allTasksCompleted = todos[userId].every(function (todo) {
-                    return (todo.completed === true);
-                });
-                userTasksStatus = (allTasksCompleted === true) ? "completedTasks" : "uncompletedTasks";
-            }
+        let userData = {
+            userId: userId,
+            name: user.name,
+            email: user.email,
+            street: user.address.street,
+            city: user.address.city,
+            zipcode: user.address.zipcode,
+            tasksStatus: userTasksStatus,
+            selectedUserId: selectedUserId
+        };
+        return <UserComp key={index}
+                         userData={userData}/>
+    });
 
-            let userData = {
-                userId:userId,
-                name:user.name,
-                email:user.email,
-                street:user.address.street,
-                city:user.address.city,
-                zipcode:user.address.zipcode,
-                tasksStatus:userTasksStatus,
-                selectedUserId:this.state.selectedUserId
-            };
-            return <UserComp key={index}
-                             userData = {userData} />
-        },this);
-
-        return (
-            <div className="ShowUsers">
-                {userComps}
-            </div>
-        );
-    }
+    return (
+        <div className="ShowUsers">
+            {userComps}
+        </div>
+    );
 }
-
-const mapStateToProps = (state) => {
-    return {myCustomProps: state};
-}
-
-export default connect(mapStateToProps)(ShowUsers);
