a254b8f9893dd8e4b1d72d554f34357c1fd78c9d
avrahamm <papaabramg@gmail.com>
2022-06-20 14:12:31 +0300

    UserPosts.js

--- src/Components/UserPosts/UserPosts.js
+++ src/Components/UserPosts/UserPosts.js
@@ -1,69 +1,47 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import * as React from 'react';
+import {useSelector} from 'react-redux';
+import {Link, useHistory, useParams, useRouteMatch} from 'react-router-dom';
+
 import PostComp from '../PostComp/PostComp';
 import './UserPosts.css';
-import {Link} from "react-router-dom";
 
-class UserPosts extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state= {userId:0, userPosts:[] };
+export default function UserPosts() {
+    const history = useHistory();
+    const match = useRouteMatch();
+    let {userId} = useParams();
+    userId = Number.parseInt(userId);
+
+    const selectedUserId = useSelector(state => state.selectedUserId);
+    //for direct loading, not by selecting user - wil be redirected.
+    if (!selectedUserId || // 0 means no user was selected - direct navigation.
+        (userId !== selectedUserId) // selected user deleted
+    ) {
+        history.push('/');
     }
 
-    static getDerivedStateFromProps(nextProps, prevState)
-    {
-        let userId = Number.parseInt(nextProps.match.params.userId);
-        let selectedUserId = nextProps.myCustomProps.selectedUserId;
-        //for direct loading not by selecting user - wil be redirected.
-        if( !selectedUserId || // 0 means no user was selected
-            (userId !== selectedUserId) //
-        ) {
-            nextProps.history.push('/');
-        }
-        let userPosts = [];
-        if ( (nextProps.myCustomProps.posts.length) &&
-            Array.isArray(nextProps.myCustomProps.posts[userId]))
-        {
-            userPosts = nextProps.myCustomProps.posts[userId];
-        }
-        return {
-            userId: userId,
-            userPosts: userPosts,
-        };
-    }
+    const userPosts = useSelector(state => state.posts[userId]);
 
-    render() {
-
-        //console.log("UserTodos render");
-        let userPostsArray = this.state.userPosts.map( (post, index) =>
-            {
-                return <PostComp key={index} post={post} />;
-            }
-        );
-        return (
-            <div className="UserPosts">
-                <div className="UserPostsHeader">
+    //console.log("UserTodos render");
+    let userPostsArray = userPosts.map((post, index) => {
+            return <PostComp key={index} post={post}/>;
+        }
+    );
+    return (
+        <div className="UserPosts">
+            <div className="UserPostsHeader">
                     <span className={"label"}>
-                        <h4>Posts - User {this.state.userId}</h4>
+                        <h4>Posts - User {userId}</h4>
                     </span>
-                    <span className={"addPostButton"}>
-                        <Link to={`${this.props.match.url}/addPost`}>
-                            <input className={"button"}  type="button" value={"Add"} />
+                <span className={"addPostButton"}>
+                        <Link to={`${match.url}/addPost`}>
+                            <input className={"button"} type="button" value={"Add"}/>
                         </Link>
                     </span>
-                </div>
-
-                <div className="UserPostsBody">
-                    {userPostsArray}
-                </div>
             </div>
-        );
-    }
-}
 
-const mapStateToProps = (state) => {
-    return {myCustomProps: state};
+            <div className="UserPostsBody">
+                {userPostsArray}
+            </div>
+        </div>
+    );
 }
-
-export default connect(mapStateToProps)(UserPosts);
